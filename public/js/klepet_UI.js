633c90e75a36e6c732475493af9b5e46211da2eb
Jernej Klarič <jernej.klari@hotmail.com>
2016-04-10 02:42:02 +0000

    Video merge

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -137,7 +137,14 @@ $(document).ready(function() {
       $('#poslji-sporocilo').focus();
     });
   });
-
+  
+  socket.on('dregljaj', function(dregljaj) 
+  {
+    $("#vsebina").jrumble();
+    $("#vsebina").trigger('startRumble');
+    setTimeout( function(){ $("#vsebina").trigger('stopRumble'); }, 1500);
+  });
+  
   setInterval(function() {
     socket.emit('kanali');
     socket.emit('uporabniki', {kanal: trenutniKanal});
